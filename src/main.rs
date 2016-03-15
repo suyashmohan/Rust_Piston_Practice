e2280bf1807aa8fb7e920f254827fa64a2d44275
Suyash Mohan <suyash@medzin.in>
2016-03-15 16:57:53 +0530

    Part 2 of Tutorial started

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,31 @@ extern crate piston_window;
 
 use piston_window::*;
 
+struct Game{
+    rotation: f64
+}
+
+impl Game{
+    fn new() -> Game{
+        Game { rotation : 0.0 }
+    }
+
+    fn on_update(&mut self, upd:UpdateArgs){
+        self.rotation += 3.0 * upd.dt;
+    }
+
+    fn on_render(&mut self, ren:RenderArgs, e:PistonWindow){
+        e.draw_2d(|c, g|{
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            let center = c.transform.trans((ren.width/2) as f64, (ren.height/2) as f64);
+            let square = rectangle::square(0.0, 0.0, 100.0);
+            let red = [1.0, 0.0, 0.0, 1.0];
+
+            rectangle(red, square, center.rot_rad(self.rotation).trans(-50.0, -50.0), g);
+        });
+    }
+}
+
 fn main() {
     let window : PistonWindow = WindowSettings::new(
         "Piston test",
@@ -10,25 +35,19 @@ fn main() {
     .exit_on_esc(true)
     .build()
     .unwrap();
-    
-    let mut rotation: f64 = 0.0;
-    
+
+    let mut game = Game::new();
+
     for e in window{
         match e.event {
-            Some(Event::Update(UpdateArgs{dt})) => {
-                rotation += 3.0 * dt;
+            Some(Event::Update(upd)) => {
+                game.on_update(upd);
+            }
+            Some(Event::Render(ren)) => {
+                game.on_render(ren, e);
             }
             _ =>{
             }
         }
-        
-        e.draw_2d(|c, g| {
-            clear([0.0, 0.0, 0.0, 1.0], g);
-            let center = c.transform.trans(400.0, 240.0);
-            let square = rectangle::square(0.0, 0.0, 100.0);
-            let red = [1.0, 0.0, 0.0, 1.0];
-            
-            rectangle(red, square, center.rot_rad(rotation).trans(-50.0, -50.0), g);
-        });
     }
 }
